9aad4d9e558f11fe2de114bbc83555343f3fbdc6
Marcus Gigandet <78104059+SlyFryFrog@users.noreply.github.com>
2023-10-21 12:25:28 -0400

    Update App.java

--- App.java
+++ App.java
@@ -32,7 +32,6 @@ public class App {
         int sum = 0;
 
         for(int i = 0; i < num.length(); i ++) {
-            System.out.println(Character.getNumericValue(num.charAt(i)));
             sum += Character.getNumericValue(num.charAt(i));
         }
         
